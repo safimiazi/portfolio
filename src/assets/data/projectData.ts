f80220341426ac18a097d0c7f0a32f7438b68247
Astro <rgrxmahabub.official@gmail.com>
2024-01-09 12:41:22 +0600

    project section added

---
+++ src/assets/data/projectData.ts
@@ -0,0 +1,70 @@
+export const projectsData = [
+    {
+        title: "Photohouse Magazine",
+        summary:
+            "Photohouse Magazine shows it’s great magazines and gallery with outstanding design and slides. Admin can upload their magazine and gallery photos. Also it’s authenticated, so it is secured. Users can see all the photos with details.",
+        projectType: "Client",
+        githubLink: "https://github.com/Astro177/Photohouse-Gallery",
+        liveLink: "https://www.photohousemagazine.com/",
+        techStack: [
+            "TypeScript",
+            "Next.js",
+            "FastAPI",
+            "Node.js",
+            "Express.js",
+            "MongoDB",
+            "Tailwind CSS",
+        ],
+    },
+    {
+        title: "Golden Peaks",
+        summary:
+            "Golden Peaks is a website for the summer camp students, it has multiple functionality for the students. This website allows it's user to select class and enroll in that class by paying for it. This website also has instructor and admin dashboard, which let them do many number of things.",
+        projectType: "Personal",
+        githubLink: "https://github.com/Astro177/Golden-Peaks-Client",
+        liveLink: "https://assignment-12-1c977.web.app/",
+        techStack: [
+            "JavaScript",
+            "React.js",
+            "FastAPI",
+            "Node.js",
+            "Express.js",
+            "MongoDB",
+            "Tailwind CSS",
+        ],
+    },
+    {
+        title: "Playful Delights",
+        summary:
+            "Playful Delights offers a user-friendly platform that allows its protected users to manage their toy inventory by adding, updating, or deleting items at their discretion. The website features an engaging animation style and vibrant colors, complemented by Firebase authentication to enhance security. The site is seamlessly connected to a secure database, ensuring the safety and confidentiality of all user data.",
+        projectType: "Personal",
+        githubLink: "https://github.com/Astro177/Playful-Delight",
+        liveLink: "https://assignment-11-a4819.web.app/",
+        techStack: [
+            "JavaScript",
+            "React.js",
+            "FastAPI",
+            "Node.js",
+            "Express.js",
+            "MongoDB",
+            "Tailwind CSS",
+        ],
+    },
+    {
+        title: "Kitchen Diaries",
+        summary:
+            "Kitchen Diaries provides a platform for its protected users to access detailed information about their chef's best recipes. The website showcases an appealing animation style and vibrant colors, incorporating Firebase authentication for enhanced security. The user experience is designed to be highly friendly, allowing users to thoroughly enjoy their time on the platform.",
+        projectType: "Personal",
+        githubLink: "https://github.com/Astro177/Kitchen-Diaries-Client.git",
+        liveLink: "https://assignment-10-2d8e2.web.app/",
+        techStack: [
+            "JavaScript",
+            "React.js",
+            "FastAPI",
+            "Node.js",
+            "Express.js",
+            "MongoDB",
+            "Tailwind CSS",
+        ],
+    },
+];
--- src/modules/DetailsSection/ProjectsSection.tsx
+++ src/modules/DetailsSection/ProjectsSection.tsx
@@ -1,51 +1,7 @@
 import React from "react";
 import AnimatedText from "../../components/AnimatedText";
 import { FaArrowRight, FaGithub } from "react-icons/fa";
-
-const projectsData = [
-    {
-        title: "LLM Agent",
-        summary:
-            "An interactive agent, able to hold a conversation with a human, and tell them about me and my professional experiences. Built using OpenAI's gpt-3.5.",
-        projectType: "Personal",
-        githubLink: "https://github.com/sifatulrabbi/openai-py-playground",
-        liveLink: "",
-        techStack: ["Python", "Langchain", "FastAPI", "Redis", "Socket.IO"],
-    },
-    {
-        title: "Social Media app",
-        summary:
-            "A social media application built with popular web technologies. The app has registration and login functionality with persisting sessions. User profile system with features such as uploading profile pictures cover photos, and following other profiles. Feed that supports posts with images, audio, and videos. Also, users can like, comment, and share posts.",
-        projectType: "Personal",
-        githubLink: "https://github.com/sifatulrabbi/social_media_app",
-        liveLink: "https://socialize-reactapp.web.app/login",
-        techStack: ["JavaScript", "React", "Node.js", "Firebase"],
-    },
-    {
-        title: "Hotel Management app",
-        summary:
-            "A Hotel Management application with backend API for a small boutique hotel. The app is responsive and fast-loading landing page with booking functionalities such as seeing a room’s availability and booking rooms. A notification system that notifies the admins when a new booking request is submitted and users when a booking request is accepted/rejected by admins. The email is also customizable by admins. Admin panel to manage booking requests, add/remove/disable rooms, or update room information.",
-        projectType: "Freelance",
-        githubLink: "https://github.com/sifatulrabbi",
-        liveLink: "",
-        techStack: [
-            "JavaScript",
-            "React",
-            "Node.js",
-            "Express.js",
-            "PostgreSQL",
-        ],
-    },
-    {
-        title: "Business Management app",
-        summary:
-            "A frontend concept application for business management tool. This tool helps businesses managing their orders, customers, and products.",
-        projectType: "Personal",
-        githubLink: "https://github.com/sifatulrabbi",
-        liveLink: "https://healthos-test.vercel.app/login",
-        techStack: ["JavaScript", "React", "Node.js"],
-    },
-];
+import { projectsData } from "../../assets/data/projectData";
 
 const ProjectsSection: React.FC = () => {
     return (
@@ -69,18 +25,20 @@ const ProjectsSection: React.FC = () => {
             <br />
             <br />
 
-            {projectsData.map((d) => (
+            {projectsData?.map((d) => (
                 <div
                     key={d.title}
-                    className="w-full prose max-w-full prose-h4:text-slate-300 prose-h4:font-medium prose-h4:my-1 text-slate-400 prose-strong:text-slate-400"
+                    className="w-full prose max-w-full prose-h4:text-slate-300 prose-h4:font-medium prose-h4:my-1 text-slate-400 prose-strong:text-slate-400 flex flex-col gap-2"
                 >
                     <small className="flex flex-row items-center gap-1 text-slate-500 font-mono">
                         {d.projectType}
                     </small>
-                    <h4 className="flex items-center gap-1">{d.title}</h4>
+                    <h4 className="flex items-center gap-1 text-2xl">
+                        {d.title}
+                    </h4>
                     <p
                         dangerouslySetInnerHTML={{ __html: d.summary }}
-                        className="my-2"
+                        className="my-2 lg:w-5/6"
                     />
                     <div className="w-full flex flex-row items-start justify-start flex-wrap gap-2">
                         {d.techStack.map((t) => (
@@ -98,7 +56,7 @@ const ProjectsSection: React.FC = () => {
                                 href={d.githubLink}
                                 target="_blank"
                                 rel="noreferrer"
-                                className="text-sm text-slate-100 flex items-center gap-1 px-4 py-2 rounded-lg bg-primary-400 lg:hover:bg-primary-600 no-underline transition-colors duration-300"
+                                className="text-sm text-slate-100 flex items-center gap-1 px-4 py-2 rounded-lg bg-primary-400 lg:hover:bg-transparent no-underline transition-colors duration-300 border border-primary-200 font-bold"
                             >
                                 GitHub
                                 <FaGithub className="text-base" />
@@ -109,7 +67,7 @@ const ProjectsSection: React.FC = () => {
                                 href={d.liveLink}
                                 target="_blank"
                                 rel="noreferrer"
-                                className="text-sm text-primary-400 lg:hover:text-primary-200 flex items-center gap-1 px-4 py-2 rounded-lg border border-primary-400 lg:hover:border-primary-200 no-underline transition-colors duration-300"
+                                className="text-sm text-primary-400 font-bold flex items-center gap-1 px-4 py-2 rounded-lg border border-primary-400 hover:bg-primary-400 hover:text-white lg:hover:border-primary-200 no-underline transition-colors duration-300"
                             >
                                 Live link
                                 <FaArrowRight className="text-sm -rotate-45" />
